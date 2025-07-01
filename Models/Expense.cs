e321d15530974b5caab500c28749db4de84214f7
Sachith Priyashantha <sachithpriyashantha24@gmail.com>
2025-07-01 23:24:55 +0530

    Create the Finance Model

---
+++ Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Range(0.01, double.MaxValue, ErrorMessage = "AMount is not in the given range")] 
+        [Required]
+        public double Amount { get; set; }
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
