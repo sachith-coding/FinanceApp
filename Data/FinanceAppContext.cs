2616826db5d5800633f718b2b83683370a3a3630
Sachith Priyashantha <sachithpriyashantha24@gmail.com>
2025-07-02 22:18:37 +0530

    Set Connection with Database in MySQL Workbench

---
+++ Data/FinanceAppContext.cs
@@ -0,0 +1,12 @@
+﻿using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options): base(options) { }
+
+        DbSet<Expense> Expenses { get; set; } // table of the DB
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using FinanceApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<FinanceAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
